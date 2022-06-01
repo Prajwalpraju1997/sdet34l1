76ccabf0f90b3e06f09ef5f2fe09d115e58672c5
Prajwalpraju1997 <103984488+Prajwalpraju1997@users.noreply.github.com>
2022-06-01 10:08:07 +0530

    Update BasicTestNgConfigAnnotation.java

--- src/test/java/Com/vtiger/practice/BasicTestNgConfigAnnotation.java
+++ src/test/java/Com/vtiger/practice/BasicTestNgConfigAnnotation.java
@@ -34,9 +34,6 @@ public class BasicTestNgConfigAnnotation {
 		Reporter.log("beforetest1", true);
 	}
 	
-	//hhfhfhf
-	vhvjj
-	
 	@AfterTest(groups = "sanity")
 	public void afterTest1Test() {
 		Reporter.log("aftertest1", true);
